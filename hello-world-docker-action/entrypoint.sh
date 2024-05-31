7c9074e949487a7ea93247ee97167de77ffdc830
Eric Hannafin <134812781+Eric-Hannafin@users.noreply.github.com>
2024-05-31 14:14:46 +0100

    Update entrypoint.sh

--- hello-world-docker-action/entrypoint.sh
+++ hello-world-docker-action/entrypoint.sh
@@ -1,1 +1,13 @@
+#!/bin/sh -l
 
+# Get the input
+name=$1
+
+# Output a greeting
+echo "Hello $name"
+
+# Get the current time
+time=$(date)
+
+# Set the output variable
+echo "::set-output name=time::$time"
